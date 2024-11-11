3347ca8db97481a7496c85da0e271d53ffb4e4b4
lucasrodlima <lucasrodlima.dev@gmail.com>
2024-11-11 02:26:20 -0300

    Cardápios repetidos não passam na validação

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,4 +2,6 @@ class Menu < ApplicationRecord
   has_many :menu_items
   has_many :products, through: :menu_items
   belongs_to :restaurant
+
+  validates :name, uniqueness: {scope: :restaurant}
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
+  describe '#valid?' do
+    it 'false if menu name is repeated in same restaurant' do
+      user = User.create!(email: 'pedro@gmail.com', password: '123456', name: 'Pedro', last_name: 'Pereira', cpf: '57136336163')
+      place = Restaurant.create!(brand_name: 'TIM', corporate_name: 'Tim ltda', cnpj: "E67A879U2DOS80", address: 'Rua São Pedro 1234, São Paulo/SP', phone_number: "9180088008", user: user, code: 'EYFFKJ')
+      Menu.create!(name:"Jantar", restaurant: place)
+      menu = Menu.new(name:"Jantar", restaurant: place)
+      
+      menu.valid?
+
+      expect(menu.errors).to include :name
+    end
+  end
 end
